db57d0408640b2968b75ea2cda91b9a31e61cf47
Alyona Chernova <Alyona.Chernova@mckesson.com>
2019-08-26 19:13:50 +0600

    fix for scrolling

--- src/test/java/testClasses/AuthorFilter.java
+++ src/test/java/testClasses/AuthorFilter.java
@@ -3,11 +3,11 @@ package testClasses;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.Random;
 
 
@@ -20,37 +20,47 @@ public class AuthorFilter extends BaseTest {
     public void oneAuthorFilter() {
         String expectedName = getRandomSelectedAuthorsName();
 
-        WebDriverWait wait = new WebDriverWait(getDriver(), 60);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='f-d-select']")));
+        getToRandomPage();
 
-        java.util.List<WebElement> pages = getDriver().findElements(By.xpath("//a[@data-page]"));
-        WebElement pageNumber = pages.get(random.nextInt(pages.size()));
-        scrollToElement(pageNumber);
-        wait.until(ExpectedConditions.elementToBeClickable(pageNumber));
-        pageNumber.click();
+        clickOnRandomBookCard();
 
-        wait.until(element -> element.findElement(By.xpath("//div[@id='content']/descendant-or-self::*[contains(@src,'.jpg')]")).isEnabled());
-
-        java.util.List<WebElement> booksList = getDriver().findElements(By.xpath("//div[@id='content']/descendant-or-self::*[contains(@src,'.jpg')]"));
-        System.out.println(booksList.size());
-        WebElement bookItem = booksList.get(random.nextInt(booksList.size()));
-
-        wait.until(element -> bookItem.isEnabled());
-
-        bookItem.click();
         String bookAuthor = getDriver().findElement(By.xpath("//p[contains(@style,'margin')]/descendant-or-self::*[contains(@href,'people')]")).getText();
         Assert.assertTrue(bookAuthor.equals(expectedName));
         findByXpathAndClick(getDriver(), "//div[contains(@class,'logo cell')]");
     }
 
-    public String getRandomSelectedAuthorsName() {
+    private String getRandomSelectedAuthorsName() {
         findByXpathAndClick(getDriver(), "//a[contains(@href,'1') and contains(text(), 'Книги')]");
         findByXpathAndClick(getDriver(), "//a[@data-filter-field-sections-id='44']");
         java.util.List<WebElement> authorsList = getDriver().findElements(By.xpath("//div[@data-filter-field-list-type='peoples']/descendant-or-self::*[contains(@data-list-found-default,'true')]"));
         WebElement element = authorsList.get(random.nextInt(authorsList.size()));
         String expectedName = element.getAttribute("data-list-found-name");
         element.click();
+        waitUntilClickable(getDriver().findElement(By.xpath("//div[@class='f-d-select']")));
         return expectedName;
+    }
+
+    private void getToRandomPage(){
+        int actualNumber = getDriver().findElements(By.xpath("//a[@data-page]")).size();
+        int randomNumber = new Random().ints(1, actualNumber + 1).findFirst().getAsInt();
+
+        String xpath = "//a[@data-page]["+ randomNumber +"]";
+        waitUntilSearchIsReady();
+        scrollToTheEndOfPage();
+        waitUntilClickable(getDriver().findElement(By.xpath(xpath)));
+        scrollToTheEndOfPage();
+        getDriver().findElement(By.xpath(xpath)).click();
+        WebDriverWait wait = new WebDriverWait(getDriver(), 60);
+        wait.until(element -> element.findElement(By.xpath("//div[@id='content']/descendant-or-self::*[contains(@src,'.jpg')]")).isEnabled());
+    }
+
+    private void clickOnRandomBookCard(){
+        List<WebElement> booksList = getDriver().findElements(By.xpath("//div[@class='placeholder']//a[@class='title']"));
+        System.out.println(booksList.size());
+        WebElement bookItem = booksList.get(random.nextInt(booksList.size()));
+        waitUntilClickable(bookItem);
+        scrollToElement(bookItem);
+        bookItem.click();
     }
 
 }
--- src/test/java/testClasses/BaseTest.java
+++ src/test/java/testClasses/BaseTest.java
@@ -2,9 +2,12 @@ package testClasses;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
@@ -31,8 +34,8 @@ public class BaseTest {
         //driver = new FirefoxDriver();
         System.setProperty("webdriver.chrome.driver", "src/test/java/resources/chromedriver76.exe");
         driver = new ChromeDriver();
-        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         getToPage(driver, "https://flip.kz");
         return driver;
@@ -68,9 +71,8 @@ public class BaseTest {
 
     @AfterSuite
     public void afterSuite() {
-      //  driver.close();
+        //  driver.close();
     }
-
 
     private static void getToPage(WebDriver driver, String URL) {
         driver.get(URL);
@@ -80,13 +82,35 @@ public class BaseTest {
         driver.findElement(By.xpath(xpath)).click();
     }
 
-    protected void waitUntilClickable(WebElement element){
+    protected void waitUntilClickable(WebElement element) {
         WebDriverWait wait = new WebDriverWait(getDriver(), 60);
         wait.until(ExpectedConditions.elementToBeClickable(element));
     }
 
-    protected void scrollToElement(WebElement element){
-        JavascriptExecutor jse = (JavascriptExecutor)getDriver();
-        jse.executeScript("arguments[0].scrollIntoView(true)", element);
+    protected void scrollToElement(By by) {
+        JavascriptExecutor jse = (JavascriptExecutor) getDriver();
+        jse.executeScript("arguments[0].scrollIntoView(false)", getDriver().findElement(by));
+    }
+
+    protected void scrollToElement(WebElement webElement) {
+        JavascriptExecutor jse = (JavascriptExecutor) getDriver();
+        jse.executeScript("arguments[0].scrollIntoView(false)", webElement);
+    }
+
+    protected void scrollToTheEndOfPage() {
+        JavascriptExecutor jse = (JavascriptExecutor) getDriver();
+        jse.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+    }
+
+    protected void waitUntilSearchIsReady() {
+        new WebDriverWait(getDriver(), 60)
+                .ignoring(StaleElementReferenceException.class, WebDriverException.class)
+                .until(ExpectedConditions.attributeToBe(getDriver().findElement(By.xpath(".//div[@id='content']")),
+                        "style", "opacity: 1;"));
+    }
+
+    protected void waitPageForLoad() {
+        new WebDriverWait(driver, 30).until((ExpectedCondition<Boolean>) wd ->
+                ((JavascriptExecutor) wd).executeScript("return document.readyState").equals("complete"));
     }
 }
